70ca92b4d949eada88d4d3af89c6c2bb877cd14c
McMmie <tharausamuel01@gmail.com>
2023-02-17 14:24:35 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,10 +7,6 @@
 
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
