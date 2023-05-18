47d3a3f20be0a81f8de4faadd0f56fef045e406d
McMmie <tharausamuel01@gmail.com>
2023-05-18 09:27:30 +0000

    fixed errors on quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
 
 int main(void)
 {
-	char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char buf[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
 
-	write(2,buf, buf[sizeof(strlen(buf))]);
+	write(2, buf, 59);
 	return (1);
 }
