92309d5815254163a69d5edcfc973b7d9e70fdb3
McMmie <tharausamuel01@gmail.com>
2023-05-18 09:02:45 +0000

    all files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - using printf to print
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		return (0);
+}
