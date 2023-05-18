ab6338985e38698c9a4268eb2b2e7c9a7d32435f
McMmie <tharausamuel01@gmail.com>
2023-05-18 08:49:14 +0000

    scripts and source files

---
+++ 101-quote.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - prints to stderr
+ *
+ * Return: 1 (success)
+ */
+
+int main(void)
+{
+	char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2,buf, buf[sizeof(strlen(buf))]);
+	return (1);
+}
