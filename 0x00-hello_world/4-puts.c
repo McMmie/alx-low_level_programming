2a0eda95d3ac0a2d49889c060b154cfa4caf2ed8
McMmie <tharausamuel01@gmail.com>
2023-05-18 09:14:00 +0000

    removed the extra character

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - prints using puts
@@ -8,7 +9,7 @@
 
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle\n";
+	char str[52] = "\"Programming is like building a multilingual puzzle\n";
 		puts(str); /*writing to standard output*/
 
 		return (0);
