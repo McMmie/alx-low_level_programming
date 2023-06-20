f8e55f3b770f56db0c3244739e44bb9d278b4025
McMmie <tharausamuel01@gmail.com>
2023-06-20 09:34:30 +0000

    fixed betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
+/**
+ * struct dog - defines a new type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Description: It creates a new struct that contains
+ * information about a dog, its age and its owner
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
