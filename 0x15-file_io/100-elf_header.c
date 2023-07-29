f8ac8bee7a35670ad2c515704b6f0f6925d9d375
McMmie <tharausamuel01@gmail.com>
2023-07-29 21:36:05 +0000

    update files

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -36,16 +36,16 @@ void elf_header_info(const header *head)
 		}
 		 printf("\n");
 
-		printf("Class:\t\t\t			%s\n",
-			(head->e_ident[4] == 1) ? "ELF32" :
-			((head->e_ident[4] == 2) ? "ELF64" : "Unknown"));
+		printf("Class:                               %s\n",
+			(head->e_ident[4] == 1) ? "\t\t\tELF32" :
+			((head->e_ident[4] == 2) ? "\t\t\tELF64" : "Unknown"));
 		printf("Data:\t\t\t				%s\n",
 			(head->e_ident[5] == 1) ?
-			"2's complement, Little Endian" : ((head->e_ident[5] == 2) ?
-			"2's complement, Big Endian" : "unknown"));
-		printf("Version:\t\t\t			%s\n",
+			"\t\t\t2's complement, Little Endian" : ((head->e_ident[5] == 2) ?
+			"\t\t\t2's complement, Big Endian" : "unknown"));
+		printf("Version:                                  %s\n",
 			(head->e_ident[6] == 1) ? "1 (Current)" : "Unknown");
-		printf("OS/ABI:\t\t\t					");
+		printf("OS/ABI:                                    ");
 		switch (head->e_ident[7])
 		{
 			case 0:
@@ -96,7 +96,7 @@ void elf_header_info(const header *head)
 		default:
 			printf("<unknown: %x>\n", head->e_type);
 	}
-	printf("  Entry point address:				%lx\n",
+	printf("  Entry point address:				0x%lx\n",
 	head->e_entry);
 }
 
