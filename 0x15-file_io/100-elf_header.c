11e28ad4ca0295b531f921bc3458c1b65c27b0b1
McMmie <tharausamuel01@gmail.com>
2023-07-29 21:08:40 +0000

    updated files

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -34,14 +34,68 @@ void elf_header_info(const header *head)
 		}
 		 printf("\n");
 
-		printf("Class: %d\n", head->e_ident[4]);
-		printf("Data: %d\n", head->e_ident[5]);
-		printf("Version: %d\n", head->e_ident[6]);
-		printf("OS/ABI: %d\n", head->e_ident[7]);
-		printf("ABI Version: %d\n", head->e_ident[8]);
+		printf("Class:				%s\n",
+			(head->e_ident[4] == 1) ? "ELF32" :
+			((head->e_ident[4] == 2) ? "ELF64" : "Unknown"));
+		printf("Data:				%s\n",
+			(head->e_ident[5] == 1) ?
+			"2's complement, Little Endian" : ((head->e_ident[5] == 2) ?
+			"2's complement, Big Endian" : "unknown"));
+		printf("Version:			%s\n",
+			(head->e_ident[6] == 1) ? "1 (Current)" : "Unknown");
+		printf("OS/ABI:					");
+		switch (head->e_ident[7])
+		{
+			case 0:
+				printf("UNIX - System V");
+				break;
+			case 1:
+				printf("UNIX - HP-UX");
+				break;
+			case 2:
+				printf("UNIX - NetBSD");
+				break;
+			case 3:
+				printf("UNIX - Linux");
+				break;
+			case 6:
+				printf("UNIX - Sun Solaris");
+				break;
+			case 9:
+				printf("UNIX - FreeBSD");
+				break;
+			case 12:
+				printf("UNIX - OpenBSD");
+				break;
+			default:
+				printf("<Unknown>");
+		}
 
-		printf("Type: %d\n", head->e_type);
-		printf("Entry point address: 0x%lx\n", head->e_entry);
+		printf("  ABI Version:                       %d\n",
+	head->e_ident[8]);
+	printf("  Type:                              ");
+	switch (head->e_type)
+	{
+		case 0:
+			printf("NONE (No file type)\n");
+			break;
+		case 1:
+			printf("REL (Relocatable file)\n");
+			break;
+		case 2:
+			printf("EXEC (Executable file)\n");
+			break;
+		case 3:
+			printf("DYN (Shared object file)\n");
+			break;
+		case 4:
+			printf("CORE (Core file)\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", head->e_type);
+	}
+	printf("  Entry point address:				%#lx\n",
+	(unsigned long) head->e_entry);
 }
 
 /**
