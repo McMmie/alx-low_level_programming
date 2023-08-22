868e6d102a288e0957959d25d3c235f052ab8fbf
McMmie <tharausamuel01@gmail.com>
2023-08-22 08:37:29 +0000

    resolved conflict

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -c -fPIC *.c | gcc -shared -o liball.so *.o
+gcc -c -fPIC *.c | gcc -shared -o liball.so *.o | export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
--- 0x18-dynamic_libraries/div.c
+++ 0x18-dynamic_libraries/div.c
@@ -1,4 +1,4 @@
 int div(int a, int b)
 {
-	return(a * b);
+	return(a / b);
 }
