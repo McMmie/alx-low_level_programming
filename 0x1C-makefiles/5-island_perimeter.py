48e6a518bca250a5e4608f648413e5c123c73f92
McMmie <tharausamuel01@gmail.com>
2023-10-25 23:42:29 +0000

    technical question

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Technical question module
+"""
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in grid
+    """
+    p = 0
+    count = 0
+    for rows in grid:
+        for digit in rows:
+            if digit == 1:
+                count += 1
+
+    if count == 1:
+        p = 4
+    else:
+        p = (count * 2) + 2
+
+
+    return p
