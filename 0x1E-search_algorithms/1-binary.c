6cd3115c00fbe953fc549f43e2b911a67e1faf5f
McMmie <tharausamuel01@gmail.com>
2023-12-12 17:14:19 +0000

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,34 +14,10 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	/* size_t i;
-	int left = 0;
-	int right = size - 1;
-	int mid = (left + right) / 2;
-*/
+
 	if (array == NULL)
 		return (-1);
 
-	return binary_search_recursive(array, 0, size - 1, value);
+	return (binary_search_recursive(array, 0, size - 1, value));
 
-/*
-	for (i = 0; i < size; i++)
-	{
-		if (i == size - 1)
-			printf("Searching in array: %d\n", array[i]);
-		else
-			printf("Searching in array: %d,", array[i]);
-	}
-	
-	if (value == array[mid])
-		return mid;
-	
-	if (array[mid] > value)
-		return (binary_search(array, mid - 1, value));
-	else
-		return (binary_search(array, mid + 1, value));
-
-
-	return (-1);
-*/
 }
--- 0x1E-search_algorithms/1.c
+++ 0x1E-search_algorithms/1.c
@@ -15,26 +15,26 @@
 
 int binary_search_recursive(int *array, int left, int right, int value)
 {
-    int mid = left + (right - left) / 2;
-    int i = 0;
+	int mid = left + (right - left) / 2;
+	int i = 0;
 
-    if (left > right)
-        return -1; 
+	if (left > right)
+		return (-1);
 
-    printf("Searching in array: ");
-    for (i = left; i <= right; i++)
-    {
-        if (i == right)
-            printf("%d\n", array[i]);
-        else
-            printf("%d, ", array[i]);
-    }
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		if (i == right)
+			printf("%d\n", array[i]);
+		else
+			printf("%d, ", array[i]);
+	}
 
-    if (array[mid] == value)
-        return mid;
+	if (array[mid] == value)
+		return (mid);
 
-    if (array[mid] > value)
-        return binary_search_recursive(array, left, mid - 1, value);
+	if (array[mid] > value)
+		return (binary_search_recursive(array, left, mid - 1, value));
 
-    return binary_search_recursive(array, mid + 1, right, value);
+	return (binary_search_recursive(array, mid + 1, right, value));
 }
